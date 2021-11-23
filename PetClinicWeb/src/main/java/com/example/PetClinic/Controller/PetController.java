e5ea8d834244236ef89d98671e1f2a5b690c809d
KapilChaudhary <KapilChaudhary706@gmail.com>
2021-11-23 19:09:22 +0530

    Adding PetClinic Index page closes #8

---
+++ PetClinicWeb/src/main/java/com/example/PetClinic/Controller/PetController.java
@@ -0,0 +1,15 @@
+package com.example.PetClinic.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PetController {
+	
+	@RequestMapping({"","/","index","index.html"})
+	public String index() {
+		return "index";
+	}
+	
+
+}
